b2ad9c1fc6c7f0f6ac41b354ffd4ce6d1766c77b
dufourtheo <theodufour.promail@gmail.com>
2023-01-22 21:05:21 +0100

    update

--- src/Controller/CtfController.php
+++ src/Controller/CtfController.php
@@ -36,6 +36,34 @@ class CtfController extends AbstractController
             'controller_name' => 'CtfController',
         ]);
     }
+    /**
+     * @Route("/programmation", name="programmation")
+     */
+    public function programmation(): Response
+    {
+        return $this->render('ctf/programmation.html.twig', [
+            'controller_name' => 'CtfController',
+        ]);
+    }
+    /**
+     * @Route("/stegano", name="stegano")
+     */
+    public function stegano(): Response
+    {
+        return $this->render('ctf/stegano.html.twig', [
+            'controller_name' => 'CtfController',
+        ]);
+    }
+    /**
+     * @Route("/reseaux", name="reseaux")
+     */
+    public function reseaux(): Response
+    {
+        return $this->render('ctf/reseaux.html.twig', [
+            'controller_name' => 'CtfController',
+        ]);
+    }
+
 
 
 
